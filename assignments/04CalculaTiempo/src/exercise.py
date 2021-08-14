647e398fc3e58959f07d4a2b0f098afea04d0b95
AndreaBarrantesB <A01742350@itesm.mx>
2021-08-14 00:18:58 +0000

    Ejercicios resueltos 2

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,9 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
-
+    año = int(input("Dame el año actual: "))
+    edad = int(input("Dame tu edad: "))
+    resta = (100-edad)
+    resultado = (resta+año)
+    print("Cumplirás 100 años en el año:",resultado)
 
 if __name__ == '__main__':
     main()
--- assignments/05Lustros/src/exercise.py
+++ assignments/05Lustros/src/exercise.py
@@ -1,8 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+    nacimiento = float(input("Dame el año de nacimiento: "))
+    actualidad = float(input("Dame el año actual: "))
+    Lustro = (actualidad-nacimiento)/5
+    print("Los lustros que has vivido son:",Lustro)
 
 if __name__ == '__main__':
     main()
